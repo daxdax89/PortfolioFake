65ccfa95004def3e6dacb48ebfeb8395b4aad096
daxdax89 <daxdax89@msn.com>
2017-12-02 17:48:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,296 @@
+ <!DOCTYPE HTML>
+ <html>
+ <head>
+ 	<title>DaX</title>
+ 	<meta charset="utf-8" />
+ 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+ 	<meta name="description" content="Web Developer Porftolio">
+ 	<meta name="keywords" content="web developer, web programmer, web designer, wordpress, wordpress developer, coder, website developer">
+ 	<meta name="author" content="Damir Demirovic">
+ 	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+ 	<link rel="stylesheet" href="assets/css/main.css" />
+ 	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+ 	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+ 	<link href="hover/css/hover-min.css" rel="stylesheet" media="all">
+ 	<link rel="icon" type="image/png" href="images/favicon.png">
+ </head>
+ <body>
+
+ 	<!-- Wrapper -->
+ 	<div id="wrapper">
+
+ 		<!-- Header -->
+ 		<header id="header">
+ 			<div class="inner">
+
+ 				<!-- Logo -->
+ 				<a href="index.html" class="logo">
+ 					<span class="symbol"><img src="images/favicon.png" alt="" /></span><span class="title">daxdax89.com</span>
+ 				</a>
+
+ 				<!-- Nav -->
+ 				<nav style="visibility: hidden;">
+ 					<ul>
+ 						<li><a href="#menu">Menu</a></li>
+ 					</ul>
+ 				</nav>
+
+ 			</div>
+ 		</header>
+
+ 		<!-- Menu -->
+ 		<nav id="menu" style="visibility: hidden;">
+ 			<h2>Menu</h2>
+ 			<ul>
+ 				<li><a href="index.html">Home</a></li>
+ 				<li><a href="#aboutMe">About Me</a></li>
+ 				<li><a href="#projects">Projects</a></li>
+ 				<li><a href="generic.html">Contact Me</a></li>
+ 				<li><a href="elements.html">Elements</a></li>
+ 			</ul>
+ 		</nav>
+
+ 		<!-- Main -->
+ 		<div id="main">
+ 			<div class="inner">
+ 				<header>
+ 					<h1>Hello, i am <span style="color: #337ab7;margin-bottom: 1%;" class="hvr-float-shadow"> Dax</span>. Welcome to my website.</h1>
+ 					<p>Learn more about me, check out my previous projects, hire me if you you like my skills or just contact me to say hi!</p>
+ 				</header>
+
+ 				<div id="aboutMe" class="wrapMoj">
+ 					<img src="images/about.png" style="width: 100%;position: relative;z-index: 1;">
+ 					<h2 style="margin: 1%;">General info</h2>
+ 					<hr class="style14">
+ 					<p>	 					
+ 						I am a 27 years old web developer and coder. I love technology, i love coding, i love music and sport. I love gadgets. I can't imagine my life without my smartphone. I am Android fan. I prefer Linux over Windows. Never used Mac. I love PS4 more than xBox. I support Juventus. Forza Juve! I am a passionated coder from Serbia living on internet. I got interested into coding since i was 9 years old but i never really had opportunity to advance and also my parents never had resources to pay quality education for me so everything i know i've learned right from you guys! Gotta love the true power of The Internet!
+ 					</p>
+ 					<h2 style="margin: 1%;">Coding Direction</h2>
+ 					<hr class="style14">
+ 					<p>
+ 						When i was 9 years old i started with a very small programming language called Basic. I had my first old computer that had huge keyboard and green monitor. It could save data on music tape! It was capable of writing some small and fun programs. I didn't know English well back then and my first coding steps were with a help of my dad, who also didn't know anything about coding back then. He still doesn't. I owe him a huge "Thank you!" for making me interested into programming. Later we got Playstation 1 so i've never really continued coding. Although i did miss it. It wasn't long till i got into a highschool which brought back some minor coding classes. After highschool i dropped out of the college and decided to learn what i love. So here i am today being able to develop websites and code software all on my own. Coding it's my passion, i live though it and i will always have abnormal urge to keep learning and try to become a better programmer.
+ 					</p>
+ 					<hr>
+ 				</div>
+ 				<div>
+ 					<img src="images/projects.png" style="width: 100%;position: relative;z-index: 3;">
+ 				</div>
+ 				<hr>
+ 				<section class="tiles">
+ 					<article id="exodious" class="style1">
+ 						<span class="image">
+ 							<img src="images/Screenshot_1.png" alt="exodious">
+ 						</span>
+ 						<a href="http://exodious.com" target="_blank">
+ 							<h2>Exodious</h2>
+ 							<div class="content">
+ 								<p>Dating agency website.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style2">
+ 						<span class="image">
+ 							<img src="images/port.png" alt="portfolio" />
+ 						</span>
+ 						<a href="http://daxdax89.com/protofolio/" target="_blank">
+ 							<h2>Portfolio</h2>
+ 							<div class="content">
+ 								<p>Portfolio website.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style3">
+ 						<span class="image">
+ 							<img src="images/Screenshot_2.png" alt="quotes" />
+ 						</span>
+ 						<a href="http://quotes.daxdax89.com/" target="_blank">
+ 							<h2>Quotes</h2>
+ 							<div class="content">
+ 								<p>Relaxing quote generator i made for fun.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style4">
+ 						<span class="image">
+ 							<img src="images/ttc.png" alt="ttc" />
+ 						</span>
+ 						<a href="generic.html">
+ 							<h2>TTC</h2>
+ 							<div class="content">
+ 								<p>Technology Training Center</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style5">
+ 						<span class="image">
+ 							<img src="images/Screenshot_4.png" alt="alex" />
+ 						</span>
+ 						<a href="http://alex.daxdax89.com/" target="_blank">
+ 							<h2>Tribute Page</h2>
+ 							<div class="content">
+ 								<p>Tribute Page to my favorite player, Alessandro Del Piero</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style6">
+ 						<span class="image">
+ 							<img src="images/myles3.png" alt="spaycer" />
+ 						</span>
+ 						<a href="http://daxdax89.com/myles/" target="_blank">
+ 							<h2>Spaycer</h2>
+ 							<div class="content">
+ 								<p>Landing page for customer.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style2">
+ 						<span class="image">
+ 							<img src="images/officeSpaycer.png" alt="officesmart" />
+ 						</span>
+ 						<a href="http://daxdax89.com/myles2/">
+ 							<h2>Smarter Office</h2>
+ 							<div class="content">
+ 								<p>Office rent website.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+ 					<article class="style3">
+ 						<span class="image">
+ 							<img src="images/proof1.png" alt="blogandseo" />
+ 						</span>
+ 						<a href="http://daxdax89.com/david/landing/" target="_blank">
+ 							<h2>Blog & SEO</h2>
+ 							<div class="content">
+ 								<p>Just another landing page.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+
+ 					<article class="style1">
+ 						<span class="image">
+ 							<img src="images/proof2.png" alt="bookpage" />
+ 						</span>
+ 						<a href="http://daxdax89.com/david/book/" target="_blank">
+ 							<h2>Book Page</h2>
+ 							<div class="content">
+ 								<p>Book selling page.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+
+ 					<article class="style5">
+ 						<span class="image">
+ 							<img src="images/calc.png" alt="daxulator" />
+ 						</span>
+ 						<a href="http://daxdax89.com/daxulator/" target="_blank">
+ 							<h2>Daxulator</h2>
+ 							<div class="content">
+ 								<p>Simple JavaScript Calculator made out of boredom.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+
+
+ 					<article class="style6">
+ 						<span class="image">
+ 							<img src="images/ewp.png" alt="ewp" />
+ 						</span>
+ 						<a href="http://earnwithpes.com/" target="_blank">
+ 						<h2>Earn With PES</h2>
+ 							<div class="content">
+ 								<p>Pro Evolution Soccer betting website.</p>
+ 							</div>
+ 						</a>
+ 					</article>
+								<!--
+								<article class="style4">
+									<span class="image">
+										<img src="images/pic12.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Pretium</h2>
+										<div class="content">
+											<p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+										</div>
+									</a>
+								</article> -->
+							</section>
+						</div>
+					</div>
+
+
+
+					
+
+					<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Get in touch</h2>
+								<form method="post" action="index.php">
+									<div class="field half first">
+										<input type="text" name="name" id="name" placeholder="Name" required="">
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" required="">
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message" required=""></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="special" /></li>
+									</ul>
+								</form>
+								<?php
+								//if "email" variable is filled out, send email
+								if (isset($_REQUEST['email']))  {
+
+								//Email information
+									$admin_email = "info@daxdax89.com";
+									$email = $_REQUEST['email'];
+									$name = $_REQUEST['name'];
+									$message = $_REQUEST['message'];
+
+								//send email
+									mail($admin_email, "$name", $message, "From:" . $email);
+
+								//Email response
+									echo "Thank you for contacting me!";
+									
+								}
+
+							//if "email" variable is not filled out, display the form
+								// else  {
+								// 	?>
+
+
+								<?php
+								// }
+								?>
+							</section>
+							<section>
+								<h2>Follow</h2>
+								<ul class="icons">
+									<li><a href="https://twitter.com/object_brain" target="_blank" class="icon hvr-bob style2 fa-twitter" id="twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="https://www.facebook.com/daxdax89" target="_blank" class="icon style2 hvr-bob fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="https://www.instagram.com/daxdax89/" target="_blank" class="icon style2 hvr-bob fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="mailto:daxdax89@msn.com" class="icon style2 hvr-bob fa-envelope-o"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy;All rights reserved</li><li><i class="fa fa-code" aria-hidden="true"></i>
+								Coded with <i class="fa fa-heart hvr-pulse-grow" style="color: #d9534f;margin-bottom: 1%;" aria-hidden="true"></i>
+								by <b>DaX</b></li>
+							</ul>
+						</div>
+					</footer>
+				</div>
+
+				<!-- Scripts -->
+				<script src="assets/js/jquery.min.js"></script>
+				<script src="assets/js/skel.min.js"></script>
+				<script src="assets/js/util.js"></script>
+				<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+				<script src="assets/js/main.js"></script>
+			</body>
+			</html>
